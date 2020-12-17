bc2763e37beaedc42910a2a36632c38106867133
Jane42070 <1377219787@qq.com>
2020-12-17 21:25:50 +0800

    day12 - Break time! today's target complished

--- day12/timer.c
+++ day12/timer.c
@@ -14,7 +14,7 @@ void init_pit()
 	// 初始化的时候没有正在运行的计时器
 	timerctl.next = 0xffffffff;
 	// 设置预先设置定时器为未使用状态
-	for (i = 0; i < MAX_TIMER; i++) timerctl.timer[i].flags = 0;
+	for (i = 0; i < MAX_TIMER; i++) timerctl.timers0[i].flags = 0;
 }
 
 // 分配定时器
@@ -22,9 +22,9 @@ struct TIMER *timer_alloc()
 {
 	int i;
 	for (i = 0; i < MAX_TIMER; i++) {
-		if (timerctl.timer[i].flags == 0) {
-			timerctl.timer[i].flags = TIMER_FLAGS_ALLOC;
-			return &timerctl.timer[i];
+		if (timerctl.timers0[i].flags == 0) {
+			timerctl.timers0[i].flags = TIMER_FLAGS_ALLOC;
+			return &timerctl.timers0[i];
 		}
 	}
 	// 没有未分配的定时器
@@ -46,38 +46,52 @@ void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data)
 }
 
 // 设置定时器时间
+// 将 timer 注册到 timers 中去
+// 事先关闭中断
 void timer_settime(struct TIMER *timer, unsigned int timeout)
 {
-	// 从现在开始 timectl.count 秒后
+	int e, i, j;
 	timer->timeout = timeout + timerctl.count;
-	timer->flags = TIMER_FLAGS_USING;
-	// 更新下一时刻
-	if (timerctl.next > timer->timeout) timerctl.next = timer->timeout;
+	timer->flags = TIMER_FLAGS_ACTING;
+	e = io_load_eflags();
+	// 屏蔽其他中断
+	io_cli();
+	// 搜索注册位置
+	for (i = 0; i < timerctl.acting; i++) {
+		if (timerctl.timers[i]->timeout >= timer->timeout) break;
+	}
+	// i 号之后全部一移位
+	for (j = timerctl.acting; j < i; j--) timerctl.timers[j] = timerctl.timers[j - 1];
+	timerctl.acting++;
+	// 插入到空位上
+	timerctl.timers[i] = timer;
+	timerctl.next = timerctl.timers[0]->timeout;
+	io_store_eflags(e);
 }
 
 // 定时器中断处理
 // timeout 下一个计时器时间
 // timerctl.next 通过时间先后顺序来排列
-// 先判断当前时间（timerctl.count）是否没有到达最近的计时器时间，没有达到直接返回
-// 达到后先设置一个不可能达到的时间，遍历所有的计时器并选择激活的计时器
-// 如果当前时间大于或等于计时时间，设置计时器为已配置状态（关闭当前计时器，但不重置为未配置状态）并对计数器缓冲区发送设置的数据
-// 还未到达 timerctl.count 判断计时器时间是否合法，合法则将下一个计时器时间赋值到 timeout
+// 对活动的定时器进行移位同时将超时的定时器置为失效
+// 计算活动的定时器数量
+// 如果有活动的定时器将 timeout 设置为最近的计时器时间
+// 没有则设为默认值
 void inthandler20(int *esp)
 {
-	int i;
+	int i, j;
 	io_out8(PIC0_OCW2, 0x60);	// 把 IRQ-00 信号接收完了的信息通知给 PIC
 	timerctl.count++;
 	if (timerctl.next > timerctl.count) return;
-	timerctl.next = 0xffffffff;
-	for (i = 0; i < MAX_TIMER; i++) {
-		if (timerctl.timer[i].flags == TIMER_FLAGS_USING) {
-			if (timerctl.timer[i].timeout <= timerctl.count) {
-				timerctl.timer[i].flags = TIMER_FLAGS_ALLOC;
-				fifo8_put(timerctl.timer[i].fifo, timerctl.timer[i].data);
-			}
-			else {
-				if (timerctl.next > timerctl.timer[i].timeout) timerctl.next = timerctl.timer[i].timeout;
-			}
-		}
+	for (i = 0; i < timerctl.acting; i++) {
+		// timers 的定时器都在活动，就不设置 flags
+		if (timerctl.timers[i]->timeout > timerctl.count) break;
+		// 超时
+		timerctl.timers[i]->flags = TIMER_FLAGS_ALLOC;
+		fifo8_put(timerctl.timers[i]->fifo, timerctl.timers[i]->data);
 	}
+	// 正好有 i 个定时器超时了，其余的进行移位
+	timerctl.acting -= i;
+	for (j = 0; j < timerctl.acting; j++) timerctl.timers[j] = timerctl.timers[i + j];
+	if (timerctl.acting > 0) timerctl.next = timerctl.timers[0]->timeout;
+	else timerctl.next = 0xffffffff;
 }
