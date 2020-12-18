3841b81096d80f658b27316fe3995894006c736a
Qilin Jane <qilinjane@QilindeMacBook-Pro.local>
2020-12-18 15:03:11 +0800

    day14 - continue keyboard input

--- day14/bootpack.c
+++ day14/bootpack.c
@@ -15,12 +15,22 @@ void HariMain(void)
 	struct FIFO32 fifo;
 	// 定义背景缓冲区、鼠标缓冲区
 	unsigned char *buf_back, buf_mouse[256], *buf_win;
+	// 键盘按键字符表
+	static char keytable[0x54] = {
+		0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0,   0,
+		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[', 0,   0,   'A', 'S',
+		'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':', 0,   0,   ']', 'Z', 'X', 'C', 'V',
+		'B', 'N', 'M', ',', '.', '/', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
+		'2', '3', '0', '.'
+	};
+
 
 	unsigned int memtotal;
 	int fifobuf[128];
 	char s[40];
 	struct TIMER *timer, *timer2, *timer3;
-	int i, count = 0, mx, my;
+	int i, mx, my;
 
 	init_gdtidt();							// 初始化 全局段记录表，中断记录表
 	init_pic();								// 初始化 PIC
@@ -64,7 +74,7 @@ void HariMain(void)
 	init_mouse_cursor8(buf_mouse, 99);	// 背景色号 99
 
 	// 创建窗口
-	make_window8(buf_win, 160, 52, "Counter");
+	make_window8(buf_win, 160, 52, "window");
 
 	mx = (binfo->scrnx - 16) / 2;
 	my = (binfo->scrny - 28 - 16) / 2;
@@ -88,19 +98,25 @@ void HariMain(void)
 
 	for (;;) {
 		// 计时器开始
-		count++;
 		// sprintf(s, "%010d", timerctl.count);
 		// putfonts8_str_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s);
 		// 屏蔽其他中断
 		io_cli();
 		// 接收中断并进入等待
-		if (fifo32_status(&fifo) == 0) io_sti();
+		if (fifo32_status(&fifo) == 0) io_stihlt();
 		else {
 			i = fifo32_get(&fifo);
 			io_sti();
 			if (256 <= i && i <= 511) {
 				sprintf(s, "%02X", i - 256);
 				putfonts8_str_sht(sht_bak, 0, 0, COL8_FFFFFF, COL8_008484, s);
+				if (i < 256 + 0x54) {
+					if (keytable[i - 256] != 0) {
+						s[0] = keytable[i - 256];
+						s[1] = 0;
+						putfonts8_str_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s);
+					}
+				}
 			}
 			else if (512 <= i && i <= 767) {
 				if (mouse_decode(&mdec, i - 512) != 0) {
@@ -128,11 +144,8 @@ void HariMain(void)
 			switch (i) {
 				case 10:
 					putfonts8_str_sht(sht_bak, 0, 80, COL8_FFFFFF, COL8_008484, "10 sec");
-					sprintf(s, "%010d", count);
-					putfonts8_str_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s);
 					break;
 				case 3:
-					count = 0;
 					putfonts8_str_sht(sht_bak, 0, 64, COL8_FFFFFF, COL8_008484, "3 sec");
 					break;
 				// 模拟光标
