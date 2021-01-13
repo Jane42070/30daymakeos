d2ad1eb04aa444cdae8a0c22b3239662bf630993
Jane42070 <jql1377219787@gmail.com>
2021-01-13 23:16:02 +0800

    api_pustrwin, api_boxfilwin

--- day22/a_nask.asm
+++ day22/a_nask.asm
@@ -5,7 +5,7 @@
 
 		GLOBAL _api_putchar, _api_putstr
 		GLOBAL _api_end
-		GLOBAL _api_openwin
+		GLOBAL _api_openwin, _api_putstrwin, _api_boxfilwin
 
 [SECTION .text]
 
@@ -35,6 +35,44 @@ _api_openwin:	; int api_openwin(char *buf, int sxiz, int yxiz, int col_inv, char
 		MOV		ECX,[ESP+32]	; title
 		INT		0x40
 		POP		EBX
+		POP		ESI
+		POP		EDI
+		RET
+
+_api_putstrwin:	; void api_putstrwin(int win, int x, int y, int col, int len, char *str)
+		PUSH	EDI
+		PUSH	ESI
+		PUSH	EBP
+		PUSH	EBX
+		MOV		EDX,6
+		MOV		EBX,[ESP+20]	; win
+		MOV		ESI,[ESP+24]	; x
+		MOV		EDI,[ESP+28]	; y
+		MOV		EAX,[ESP+32]	; col
+		MOV		ECX,[ESP+36]	; len
+		MOV		EBP,[ESP+40]	; str
+		INT		0x40
+		POP		EBX
+		POP		EBP
+		POP		ESI
+		POP		EDI
+		RET
+
+_api_boxfilwin:	; void api_boxfilwin(int win, int x0, int y0, int x1, int y1, int col)
+		PUSH	EDI
+		PUSH	ESI
+		PUSH	EBP
+		PUSH	EBX
+		MOV		EDX,7
+		MOV		EBX,[ESP+20]	; win
+		MOV		EAX,[ESP+24]	; x0
+		MOV		ECX,[ESP+28]	; y0
+		MOV		ESI,[ESP+32]	; x1
+		MOV		EDI,[ESP+36]	; y1
+		MOV		EBP,[ESP+40]	; col
+		INT		0x40
+		POP		EBX
+		POP		EBP
 		POP		ESI
 		POP		EDI
 		RET
