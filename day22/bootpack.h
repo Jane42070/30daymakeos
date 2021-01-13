241c48905c3789d0a689f36b4ab5ff6c5df6c510
Jane42070 <jql1377219787@gmail.com>
2021-01-13 18:29:42 +0800

    add asm_end_app()

--- day22/bootpack.h
+++ day22/bootpack.h
@@ -38,6 +38,7 @@ void asm_inthandler21(void);
 void asm_inthandler27(void);
 void asm_inthandler2c(void);
 void asm_hrb_api();
+int asm_end_app();
 unsigned int memtest_sub(unsigned int start, unsigned int end);
 void start_app(int eip, int cs, int esp, int ds, int *tss_esp0);
 
