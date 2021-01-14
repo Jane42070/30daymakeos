f43df542911779710c7e7f0c6681fccd6b9fbd58
Jane42070 <jql1377219787@gmail.com>
2021-01-14 12:42:32 +0800

    add case 11

--- day23/terminal.c
+++ day23/terminal.c
@@ -394,6 +394,11 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 			ecx = (ecx + 0x0f) & 0xfffffff0;	// 以 16 字节为单位进位取整
 			memman_free((struct MEMMAN *) (ebx + ds_base), eax, ecx);
 			break;
+		case 11:
+			sht = (struct SHEET *) ebx;
+			sht->buf[sht->bxsize * edi + esi] = eax;
+			sheet_refresh(sht, esi, edi, esi + 1, edi + 1);
+			break;
 	}
 	return 0;
 }
