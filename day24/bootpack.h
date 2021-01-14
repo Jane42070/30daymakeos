f643bf3e4d615507907b616c6978d2e8aa748a7a
Jane42070 <jql1377219787@gmail.com>
2021-01-14 23:01:30 +0800

    change window status (focus/unfocus?)

--- day24/bootpack.h
+++ day24/bootpack.h
@@ -320,6 +320,7 @@ void task_idle();								// 闲置任务
 /* window.c */
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
 void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
+void change_wtitle8(struct SHEET *sht, char act);
 void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
 
 /* terminal.c */
