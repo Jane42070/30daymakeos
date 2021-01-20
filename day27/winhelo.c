49a4bb9e7d497d7846e3bc7dd784cab2dc481381
Jane42070 <jql1377219787@gmail.com>
2021-01-20 20:33:49 +0800

    any key to quit

--- day27/winhelo.c
+++ day27/winhelo.c
@@ -1,4 +1,5 @@
 int api_openwin(char *buf, int sxiz, int yxiz, int col_inv, char *title);
+int api_getkey(int mode);
 void api_end();
 
 char buf[150 * 50];
@@ -7,5 +8,6 @@ void HariMain()
 {
 	int win;
 	win = api_openwin(buf, 150, 50, -1, "Hello");
+	api_getkey(1);
 	api_end();
 }
--- day27/winhelo2.c
+++ day27/winhelo2.c
@@ -1,6 +1,7 @@
 int api_openwin(char *buf, int sxiz, int yxiz, int col_inv, char *title);
 void api_putstrwin(int win, int x, int y, int col, int len, char *str);
 void api_boxfilwin(int win, int x0, int y0, int x1, int y1, int col);
+int api_getkey(int mode);
 void api_end();
 
 char buf[150 * 50];
@@ -11,5 +12,6 @@ void HariMain()
 	win = api_openwin(buf, 150, 50, -1, "Hello2");
 	api_boxfilwin(win, 8, 36, 141, 43, 3);// 黄色
 	api_putstrwin(win, 28, 28, 0, 12, "Hello, world");
+	api_getkey(1);
 	api_end();
 }
