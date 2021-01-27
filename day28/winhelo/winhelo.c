b3fc4766c9f16a99f242f4f74096461f992b8923
Jane42070 <jql1377219787@gmail.com>
2021-01-27 16:22:49 +0800

    change winhelos to use stack instead of program itself

--- day28/winhelo/winhelo.c
+++ day28/winhelo/winhelo.c
@@ -1,10 +1,9 @@
 #include "../apilib.h"
 
-char buf[150 * 50];
-
 void HariMain()
 {
 	int win;
+	char buf[150 * 50];
 	win = api_openwin(buf, 150, 50, -1, "Hello");
 	for (;;) {
 		if (api_getkey(1) == 0x1c) break;
--- day28/winhelo2/winhelo2.c
+++ day28/winhelo2/winhelo2.c
@@ -1,10 +1,9 @@
 #include "../apilib.h"
 
-char buf[150 * 50];
-
 void HariMain()
 {
 	int win;
+	char buf[150 * 50];
 	win = api_openwin(buf, 150, 50, -1, "Hello2");
 	api_boxfilwin(win, 8, 36, 141, 43, 3);// 黄色
 	api_putstrwin(win, 28, 28, 0, 12, "Hello, world");
