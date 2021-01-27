c3b529f892420e1d74e3fcc1b794b941c20b118b
Jane42070 <jql1377219787@gmail.com>
2021-01-27 19:28:09 +0800

    cat app

--- day28/cat/cat.c
+++ day28/cat/cat.c
@@ -4,14 +4,20 @@ void HariMain()
 {
 	int fh;
 	char c;
-	fh = api_fopen("ipl10.asm");
+	char cmdline[30], *p;
+	api_cmdline(cmdline, 30);
+	// 跳过之前的内容，直到遇到空格
+	for (p = cmdline; *p > ' '; p++) {  }
+	// 跳过空格
+	for (; *p == ' '; p++) {  }
+	fh = api_fopen(p);
 	if (fh != 0) {
 		for (;;) {
-			if (api_fread(&c, 1, fh) == 0) {
-				break;
-			}
+			if (api_fread(&c, 1, fh) == 0) break;
 			api_putchar(c);
 		}
+	} else {
+		api_putstr("File not found.");
 	}
 	api_end();
 }
